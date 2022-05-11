5e08a75f5fc7bc0c94617ec742e17af5d7bbb6a4
unknown <elfraera.winson@ui.ac.id>
2022-05-11 23:29:13 +0700

    add form registrasi dan login

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,7 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('registeradmin', views.registeradmin, name='registeradmin'),
+    path('registerpengguna', views.registerpengguna, name='registerpengguna'),
+    path('login', views.login, name='login')    
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from .forms import CreateUserForm
+from .forms2 import CreateUserForm2
 
 # Create your views here.
 
 def index(request):
     return render(request, 'home/index.html')
+
+def registeradmin(request):
+    form = CreateUserForm(request.POST or None)
+    context = {'form':form}
+    return render(request, 'home/registeradmin.html', context)
+
+def registerpengguna(request):
+    form = CreateUserForm2(request.POST or None)
+    context = {'form':form}
+    return render(request, 'home/registerpengguna.html', context)
+
+def login(request):
+    form = CreateUserForm(request.POST or None)
+    context = {'form':form}
+    return render(request, 'home/login.html', context)
