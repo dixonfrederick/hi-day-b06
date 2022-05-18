025bf818d615fab21d841a8c41eaeeab6832dce7
unknown <elfraera.winson@ui.ac.id>
2022-05-18 18:27:01 +0700

    fix historitanaman for tk4

--- historitanaman/views.py
+++ historitanaman/views.py
@@ -1,3 +1,4 @@
+import email
 from collections import namedtuple
 import imp
 from django.http.response import HttpResponseNotFound, HttpResponseRedirect
@@ -7,8 +8,6 @@ from django.db.utils import IntegrityError, InterfaceError
 from .forms import *
 
 # Create your views here.
-role = ""
-
 def namedtuplefetchall(cursor):
     desc = cursor.description
     nt_result = namedtuple('Result', [col[0] for col in desc])
@@ -21,12 +20,13 @@ def createhistoritanamanpengguna(request):
 
 def readhistoritanamanadmin(request):
     cursor = connection.cursor()
-    cursor.execute("SET SEARCH_PATH to public")
+    cursor.execute("SET search_path TO public")
     role = request.session ['role']
+    userEmail = request.session['email']
     if (role == "admin"):
         try:
             cursor.execute("SET SEARCH_PATH TO hidayb06")
-            cursor.execute("SELECT histori_produksi.Email, histori_produksi.Waktu_Awal, histori_produksi.Waktu_Selesai, histori_produksi.Jumlah, histori_produksi.XP FROM histori_produksi, histori_tanaman WHERE histori_produksi.Email = histori_tanaman.Email AND histori_produksi.Waktu_Awal = histori_tanaman.Waktu_Awal")
+            cursor.execute("""SELECT HISTORI_TANAMAN.EMAIL, HISTORI_TANAMAN.WAKTU_AWAL, HISTORI_PRODUKSI.WAKTU_SELESAI, HISTORI_PRODUKSI.JUMLAH, HISTORI_PRODUKSI.XP, PETAK_SAWAH.JENIS_TANAMAN FROM HISTORI_TANAMAN, HISTORI_PRODUKSI, PETAK_SAWAH WHERE HISTORI_TANAMAN.ID_BIBIT_TANAMAN = PETAK_SAWAH.ID_ASET AND EXISTS (SELECT * FROM HISTORI_PRODUKSI WHERE HISTORI_PRODUKSI.EMAIL = HISTORI_TANAMAN.EMAIL AND HISTORI_PRODUKSI.WAKTU_AWAL = HISTORI_TANAMAN.WAKTU_AWAL);""")
             result = namedtuplefetchall(cursor)
         except Exception as e:
             print(e)
@@ -35,12 +35,13 @@ def readhistoritanamanadmin(request):
 
 def readhistoritanamanpengguna(request):
     cursor = connection.cursor()
-    cursor.execute("SET SEARCH_PATH to public")
+    cursor.execute("SET search_path TO public")
     role = request.session ['role']
+    userEmail = request.session['email']
     if (role == "pengguna"):
         try:
             cursor.execute("SET SEARCH_PATH TO hidayb06")
-            cursor.execute("SELECT histori_produksi.Waktu_Awal, histori_produksi.Waktu_Selesai, histori_produksi.Jumlah, histori_produksi.XP FROM histori_produksi, histori_tanaman WHERE histori_produksi.Email = histori_tanaman.Email AND histori_produksi.Waktu_Awal = histori_tanaman.Waktu_Awal")
+            cursor.execute("""SELECT HISTORI_TANAMAN.WAKTU_AWAL, HISTORI_PRODUKSI.WAKTU_SELESAI, HISTORI_PRODUKSI.JUMLAH, HISTORI_PRODUKSI.XP, PETAK_SAWAH.JENIS_TANAMAN FROM HISTORI_TANAMAN, HISTORI_PRODUKSI, PETAK_SAWAH WHERE HISTORI_TANAMAN.ID_BIBIT_TANAMAN = PETAK_SAWAH.ID_ASET AND EXISTS (SELECT * FROM HISTORI_PRODUKSI WHERE HISTORI_PRODUKSI.EMAIL = HISTORI_TANAMAN.EMAIL AND HISTORI_PRODUKSI.WAKTU_AWAL = HISTORI_TANAMAN.WAKTU_AWAL) AND HISTORI_TANAMAN.EMAIL = %s;""", [userEmail])
             result = namedtuplefetchall(cursor)
         except Exception as e:
             print(e)
